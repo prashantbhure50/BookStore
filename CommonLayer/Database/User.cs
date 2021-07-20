e8d942ed1ed509e4506585e996d51cadc60f69f2
Prashantbhure50 <prashantbhure50@gmail.com>
2021-07-20 18:41:19 +0530

    [Prashant] Database Modle Created for User

---
+++ CommonLayer/Database/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Database
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
