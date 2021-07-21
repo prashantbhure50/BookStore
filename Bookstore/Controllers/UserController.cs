c2bd1aa6f615b1b30ece385a8e6ccfb68a2fd605
Prashantbhure50 <prashantbhure50@gmail.com>
2021-07-21 15:02:09 +0530

    [Prashant] UserController is added

---
+++ Bookstore/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        IUserBL userBl;
+        public UserController(IUserBL userBl)
+        {
+            this.userBl = userBl;
+        }
+        [HttpPost]
+        public ActionResult AddUser(Users user)
+        {
+            try
+            {
+                this.userBl.AddUser(user);
+                return this.Ok(new { success = true, message = "Registration Successful " });
+            }
+
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.Message });
+            }
+        }
+    }
+}
